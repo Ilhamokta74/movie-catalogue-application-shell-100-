18d1cee2b767543a059c1b5f3dcf258f8ff6d135
ilhamokta74 <ilhamoktavian74@gmail.com>
2022-04-11 23:13:48 +0700

    perubahan pertama

--- specs/helpers/testFactories.js
+++ specs/helpers/testFactories.js
@@ -1,10 +1,11 @@
-import LikeButtonInitiator from '../../src/scripts/utils/like-button-initiator';
+import LikeButtonPresenter from '../../src/scripts/utils/like-button-presenter';
 
 const createLikeButtonPresenterWithMovie = async (movie) => {
-  await LikeButtonInitiator.init({
+  await LikeButtonPresenter.init({
     likeButtonContainer: document.querySelector('#likeButtonContainer'),
     movie,
   });
 };
 
+// eslint-disable-next-line import/prefer-default-export
 export { createLikeButtonPresenterWithMovie };
